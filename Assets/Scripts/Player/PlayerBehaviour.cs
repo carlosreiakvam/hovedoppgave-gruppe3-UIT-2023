d89790eb310ec6c8f86eafdb8590a7ec475aba09
carlosreiakvam <cre032@uit.no>
2023-02-22 13:15:00 +0100

    Add relay implementation to PlayerBehaviour

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -8,12 +8,12 @@ public class PlayerBehaviour : NetworkBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("playerbehave");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!IsOwner) return;
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
