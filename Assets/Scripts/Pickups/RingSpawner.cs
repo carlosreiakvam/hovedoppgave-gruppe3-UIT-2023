16e0f698ecec45052af1ee9ab97ebd28c1780749
carlosreiakvam <cre032@uit.no>
2023-04-08 10:11:37 +0200

    Ring spawining

--- Assets/Scripts/Pickups/RingSpawner.cs
+++ Assets/Scripts/Pickups/RingSpawner.cs
@@ -1,22 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using QFSW.QC;
 using Unity.Netcode;
 
 public class RingSpawner : NetworkBehaviour
 {
     [SerializeField] private Transform prefab;
-    [Command]
-    private void SpawnRing()
-    {
-        if (!IsServer) return;
-        Transform enemyTransform = Instantiate(prefab);
-        enemyTransform.GetComponent<NetworkObject>().Spawn(true);
-    }
 
     public override void OnNetworkSpawn()
     {
+        Debug.Log("Spawning ring");
         if (!IsServer) return;
         Transform transform = Instantiate(prefab);
         transform.GetComponent<NetworkObject>().Spawn(true);
