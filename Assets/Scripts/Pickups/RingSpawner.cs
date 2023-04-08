d09692075b8ececf06805f1ec633fcaa904b792d
carlosreiakvam <cre032@uit.no>
2023-04-08 10:58:29 +0200

    Ring spawning with coordinates from list

--- Assets/Scripts/Pickups/RingSpawner.cs
+++ Assets/Scripts/Pickups/RingSpawner.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
 using Unity.Netcode;
+using System.Collections.Generic;
+using System;
 
 public class RingSpawner : NetworkBehaviour
 {
     [SerializeField] private Transform prefab;
+    Vector3[] spawnPoints = {
+        new Vector3(0f,0f,0f),
+        new Vector3(5f,5f,0f),
+        new Vector3(-5f,-5f,0f),
+        new Vector3(5f,-5f,0f),
+    };
 
     public override void OnNetworkSpawn()
     {
+
         Debug.Log("Spawning ring");
         if (!IsServer) return;
+        prefab.transform.position = spawnPoints[UnityEngine.Random.Range(0, spawnPoints.Length)];
         Transform transform = Instantiate(prefab);
         transform.GetComponent<NetworkObject>().Spawn(true);
     }
